0405a7eaecec067adca5edb2d29a219f4addeeab
Stephen Cerruti <scerruti@miracosta.edu>
2024-07-09 16:33:25 -0700

    Removed path for Parameterized Test CSV File
    The CSV file shoule be found on the classpath. Removing this path allows this to work in VS Code.

--- .test/TestOutput.java
+++ .test/TestOutput.java
@@ -31,7 +31,7 @@ public class TestOutput
     }
 
     @ParameterizedTest(name="{0}")
-    @CsvFileSource(resources = ".test/output_tests.csv")
+    @CsvFileSource(resources = "output_tests.csv")
     public void testOutputMatch(String testCaseName, String input, String expectedOutput, String matchType)
     {
         // Capture stdout
