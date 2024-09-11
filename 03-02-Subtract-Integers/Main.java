8f32f4241bacb68b4b6696e9bb156ae4cb1f7b67
Stephen Cerruti <scerruti@miracosta.edu>
2024-09-10 20:16:56 -0700

    Set up IntelliJ modules and removed solutions.

--- 03-02-Subtract-Integers/Main.java
+++ 03-02-Subtract-Integers/Main.java
@@ -3,24 +3,13 @@ class Main {
   public static void main(String[] args) {
 
     // First, initialize an integer variable named num1 and assign it the value 550.
-    //HIDE
     int num1 = 550;
-    //EDIT ...
-    
+
     // Next, initialize another integer variable named num2 and assign it the value 70.
-    //HIDE int num2 = 70;
-    int num2 = 70;
-    //EDIT ...
 
     // Then, declare integer variable named  answer to store the result of num1 minus num2.
-    //HIDE int answer = num1 - num2;
-    int answer = num1 - num2;
-    //EDIT ...
 
     // Finally, display the answer variable in the console, using a print statement. 
-    //HIDE System.out.println(answer);
-    System.out.println(answer);
-    //EDIT ...
-    
+
   }
 }
--- 03-06-Dividing-Doubles/Main.java
+++ 03-06-Dividing-Doubles/Main.java
@@ -3,24 +3,13 @@ class Main {
   public static void main(String[] args) {
     
     // First, declare an double variable named num1 and assign it the value 17.5.
-    //HIDE
     double num1 = 17.5;
-    //EDIT ...
-    
+
     // Next, create another double variable named num2 and assign it the value 2.5.
-    //HIDE
-    double num2 = 2.5;
-    //EDIT ...
     
     // Then, declare double variable named  answer to store the quotient of num1 divided by num2.
-    //HIDE
-    double answer = num1/num2;
-    //EDIT ...
-    
+
     // Finally, display the answer variable in the console, using a print statement. 
-    //HIDE
-    System.out.println(answer);
-    //EDIT ...
-    
+
   }
 }
